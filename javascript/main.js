65b1539fef0216cde269d7d3bac98a74e79928ec
GlendaBatres <glenda.xiomara.batres@clases.edu.sv>
2026-04-28 09:21:40 -0600

    Ejemplos y estructuras condicionales

---
+++ javascript/main.js
@@ -0,0 +1,16 @@
+//sinatxis de javascrpt
+/* otro comentario */
+
+//declarando variables y constantes
+// variable
+var nombre = "Juan Prez"; //string
+let edad = 30; //int
+let decimal = 20.2; //double/float
+let esCasado = false; //boolean
+
+// constante
+const PI = 3.1416; //double/float
+const SALUDO ="Hola Mundo!"; //string
+
+
+
